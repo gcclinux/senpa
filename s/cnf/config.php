1d25c4f783a18638d9968acb86f5c78aecbbc1eb
Ricardo Wagemaker <java@wagemaker.co.uk>
2022-01-21 12:20:01 +0000

    Create config.php

---
+++ s/cnf/config.php
@@ -0,0 +1,17 @@
+<?php
+return array (
+  'dbtype' => 'pgsql',
+  'dbhost' => 'localhost',
+  'dbport' => '5432',
+  'dbname' => 'senpa',
+  'dbuser' => 'senpa',
+  'dbpass' => 'senpa',
+  'login' => '12345678',
+  'rootURL' => 'http://localhost/senpa/',
+  'mailserver' => 'ssl://smtp.gmail.com',
+  'mailport' => '465',
+  'mailuser' => 'xxxxxxxxx',
+  'mailpass' => 'xxxxxxxxx',
+  'mailsender' => 'xxxxxxxxx',
+)
+?>
